5200e6459812d9feb1452020d1545b18b7fa5972
Klajan <48261494+Klajan@users.noreply.github.com>
2021-08-24 11:06:44 +0200

    Modified Script to also do repairs
    Elements for repairs are a bit higher on the screen for some reason

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import numpy
 import keyboard
 #from time import time, sleep
 import time
+from pynput import keyboard as pykeyboard
 from pynput.keyboard import Key, Controller
 sct = mss.mss() 
 
@@ -13,50 +14,64 @@ monitor_number = 1
 # config ends here
 
 print("Running on Monitor ", monitor_number)
-print("Press 's' to start")
-print("Once started to stop the program press 'q'")
+print("Press 's' to scrap")
+print("Press 'r' to repair")
+print("Press 'q' anytime to quit")
 is_started = False
-keyboard.wait('s')
-monitor = sct.monitors[monitor_number]
-perfect_base = cv2.imread('Ressources/perfect.png')
-start_base = cv2.imread('Ressources/start.png')
 
-# replace static bbox with dynamic
-# based on monitor = {"top": 446, "left": 646, "width": 650, "height": 200}
-# 33.65% from the left rounted down to int
-# 41.30% from the top rounted down to int
-# 33.85% width rounted up to int
-# 18.52% height rounted up to int
-monitor_bb = {
-    "left": monitor["left"] + monitor["width"] * 33 // 100,
-    "top": monitor["top"] + monitor["height"] * 41 // 100,
-    "width": monitor["width"] * 34 // 100,
-    "height": monitor["height"] * 19 // 100,
-    "mon": monitor_number
-    }
+input_keyboard = Controller()
+monitor = sct.monitors[monitor_number]
+
+
+with pykeyboard.Events() as events:
+    while True:
+        event = events.get()
+        if str(event.key) == "'q'":
+            raise SystemExit
+        if str(event.key) == "'s'":
+            monitor_bb = {
+                "left": int(monitor["left"] + monitor["width"] * 33 / 100),
+                "top": int(monitor["top"] + monitor["height"] * 41 / 100),
+                "width": int(monitor["width"] * 34 // 100),
+                "height": int(monitor["height"] * 20 // 100),
+                "mon": int(monitor_number)
+            }
+            action_base = cv2.imread('Ressources/perfect.png')
+            start_base = cv2.imread('Ressources/start_scrap.png')
+            break
+
+        if str(event.key) == "'r'":
+            monitor_bb = {
+                "left": int(monitor["left"] + monitor["width"] * 33 / 100),
+                "top": int(monitor["top"] + monitor["height"] * 32 / 100),
+                "width": int(monitor["width"] * 34 / 100),
+                "height": int(monitor["height"] * 20 / 100),
+                "mon": int(monitor_number)
+            }
+            action_base = cv2.imread('Ressources/repair.png')
+            start_base = cv2.imread('Ressources/start_repair.png')
+            break
 
 # scale images to match screen size
 # templates were captured on 1440p screen
 scale = monitor["height"] / 1440;
 if scale == 1.0:
     start = start_base
-    perfect = perfect_base
+    action = action_base
 else:
     dim_start = (
         int(round(start_base.shape[1] * scale)),
         int(round(start_base.shape[0] * scale))
         )
-    dim_perfect = (
-        int(round(perfect_base.shape[1] * scale)),
-        int(round(perfect_base.shape[0] * scale))
+    dim_action = (
+        int(round(action_base.shape[1] * scale)),
+        int(round(action_base.shape[0] * scale))
         )
     start = cv2.resize(start_base, dim_start)
-    perfect = cv2.resize(perfect_base, dim_perfect)
+    action = cv2.resize(action_base, dim_action)
 
 # start script
 print("Now running...")
-input_keyboard = Controller()
-
 while True:
     if keyboard.is_pressed('q'):
         break
@@ -69,7 +84,7 @@ while True:
             keyboard.press_and_release('space')
             is_started = True
     else:
-        result = cv2.matchTemplate(scr_remove, perfect, cv2.TM_CCOEFF_NORMED)
+        result = cv2.matchTemplate(scr_remove, action, cv2.TM_CCOEFF_NORMED)
         _, max_val, _, max_loc = cv2.minMaxLoc(result)
         if max_val > 0.92:
             input_keyboard.press(Key.space)
