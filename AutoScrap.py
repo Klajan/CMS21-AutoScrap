7a6fd1be8c5d158005dd7a9bc55039193ab3bdc1
Klajan <48261494+Klajan@users.noreply.github.com>
2021-08-26 17:43:33 +0200

    Replaced Pipe with faster shared_memory & lock
    Still has problems => 4k resolutions though

--- AutoScrap.py
+++ AutoScrap.py
@@ -1,7 +1,8 @@
 import mss
 import cv2
 import numpy
-from multiprocessing import Pipe, Process, Event, freeze_support as mp_freeze_support
+from multiprocessing import Process, Event, Lock, freeze_support as mp_freeze_support
+from multiprocessing.managers import SharedMemoryManager
 from pynput.keyboard import Key, Controller, Events as KeyEvents
 
 import time
@@ -11,21 +12,18 @@ import time
 monitor_number = 1
 # config ends here
 
-def grab(pipe, monitor, event_stop=None):
+def grab(shared_mem: tuple, lock, event_stop, monitor):
     with mss.mss() as sct:
-        i=0
-        t1 = 0
+        buf = numpy.ndarray(shared_mem[1], shared_mem[2] ,shared_mem[0].buf)
         while not event_stop.is_set():
-            t0 = time.perf_counter()
-            pipe.send(numpy.array(sct.grab(monitor))[:,:,:3])
-            i += 1
-            t1 += time.perf_counter() - t0
-            if i == 1000:
-                print(t1 / 1000)
+            lock.acquire()
+            buf[:] = numpy.array(sct.grab(monitor))
+            lock.release()
+            
 
-        print("Exiting grab worker...")
+        print("Exiting grab process...")
 
-def match(pipe, start, action, arrow):
+def match(shared_mem: tuple, lock, event_stop, start, action, arrow):
     is_started = False
     is_found = False
     match_width = action.shape[1]
@@ -37,85 +35,87 @@ def match(pipe, start, action, arrow):
     arrow_grey = arrow[:,:,2:3]
     
     active_keyboard = Controller()
-    try:
-        while True:
-            scr = pipe.recv()
-            if is_started == False:
-                # find bounding box height once
-                if match_height == 0:
-                    match_height = scr.shape[0]
-                    print(match_height)
-                result = cv2.matchTemplate(scr, start, cv2.TM_CCOEFF_NORMED)
+    scr_base = numpy.ndarray(shared_mem[1], shared_mem[2] ,shared_mem[0].buf)
+    
+    while not event_stop.is_set():
+        # wait for screenshot to be taken
+        #event_sync.wait()
+        lock.acquire()
+        if is_started == True and is_found == True:
+            scr = scr_base[0:match_y, match_x:match_x+match_width, 2:3]
+        else:
+            scr = scr_base[:,:,:3]
+        lock.release()
+        if is_started == False:
+            # find bounding box height once
+            if match_height == 0:
+                match_height = scr.shape[0]
+            result = cv2.matchTemplate(scr, start, cv2.TM_CCOEFF_NORMED)
+            _, max_val, _, max_loc = cv2.minMaxLoc(result)
+            if max_val > 0.75:
+                active_keyboard.press(Key.space)
+                active_keyboard.release(Key.space)
+                is_started = True
+        else:
+            if is_found == False:
+                # find all matches for pattern and select leftmost
+                result = cv2.matchTemplate(scr, action, cv2.TM_CCOEFF_NORMED)
+                (y, x) = numpy.where(result > 0.85)
+                if y.size > 0:
+                    is_found = True
+                    match_x = min(x)
+                    match_y = min(y)
+            else:
+                # limit search to directly above matched area in red channel and find the arrow
+                # scr_crop = scr_base[0:match_y, match_x:match_x+match_width, 2:3]
+                result = cv2.matchTemplate(scr, arrow_grey, cv2.TM_CCOEFF_NORMED)
                 _, max_val, _, max_loc = cv2.minMaxLoc(result)
-                if max_val > 0.75:
+                if max_val > 0.9:
                     active_keyboard.press(Key.space)
                     active_keyboard.release(Key.space)
-                    is_started = True
-            else:
-                if is_found == False:
-                    # find all matches for pattern and select leftmost
-                    result = cv2.matchTemplate(scr, action, cv2.TM_CCOEFF_NORMED)
-                    (y, x) = numpy.where(result > 0.85)
-                    if y.size > 0:
-                        is_found = True
-                        match_x = min(x)
-                        match_y = min(y)
-                        cv2.imwrite("test.png", scr[0:match_height, match_x:match_x+match_width, :])
-                else:
-                    # limit search to directly above matched area in red channel and find the arrow
-                    scr_crop = scr[0:match_y, match_x:match_x+match_width, 2:3]
-                    #scr_crop = scr[0:match_height, match_x:match_x+match_width, :]
-                    result = cv2.matchTemplate(scr_crop, arrow_grey, cv2.TM_CCOEFF_NORMED)
-                    _, max_val, _, max_loc = cv2.minMaxLoc(result)
-                    if max_val > 0.9:
-                        active_keyboard.press(Key.space)
-                        active_keyboard.release(Key.space)
-                        is_started = False
-                        is_found = False
-    except EOFError:
-        print("Exiting match worker...")
+                    is_started = False
+                    is_found = False
+
+    print("Exiting match process...")
 
 if __name__ == "__main__":
     # fix for broken mp if build to exe
     mp_freeze_support()
 
+
     monitor = mss.mss().monitors[monitor_number]
 
-    print("Running on Monitor ", monitor_number)
+    print("Running on Monitor", monitor_number, monitor["width"], "x", monitor["height"] )
     print("Press 's' to scrap")
     print("Press 'r' to repair")
     print("Press 'q' anytime to quit")
 
     with KeyEvents() as events:
+        top = 0
         while True:
             event_key = events.get()
             if str(event_key.key) == "'q'":
                 raise SystemExit
             if str(event_key.key) == "'s'":
-                monitor_bb = {
-                    "left": int(monitor["left"] + monitor["width"] * 33 / 100),
-                    "top": int(monitor["top"] + monitor["height"] * 41 / 100),
-                    "width": int(monitor["width"] * 34 // 100),
-                    "height": int(monitor["height"] * 20 // 100),
-                    "mon": int(monitor_number)
-                }
+                top = int(monitor["top"] + monitor["height"] * 41.00 / 100)
                 action_base = cv2.imread('Ressources/perfect.png')
                 break
             if str(event_key.key) == "'r'":
-                monitor_bb = {
-                    "left": int(monitor["left"] + monitor["width"] * 33 / 100),
-                    "top": int(monitor["top"] + monitor["height"] * 32 / 100),
-                    "width": int(monitor["width"] * 34 / 100),
-                    "height": int(monitor["height"] * 20 / 100),
-                    "mon": int(monitor_number)
-                }
+                top = int(monitor["top"] + monitor["height"] * 32.75 / 100)
                 action_base = cv2.imread('Ressources/good.png')
                 break
 
+        monitor_bb = {
+                    "left": int(monitor["left"] + monitor["width"] * 33.75 / 100),
+                    "top": top,
+                    "width": int(monitor["width"] * 32.50 / 100),
+                    "height": int(monitor["height"] * 18.50 / 100),
+                    "mon": int(monitor_number)
+                }
         start_base = cv2.imread('Ressources/start.png')
         arrow_base = cv2.imread('Ressources/arrow.png')
         scale = monitor["height"] / 1440;
-        print("Scale is: ", scale)
+        #print("Scale is: ", scale)
         if scale == 1.0:
             start = start_base
             action = action_base
@@ -138,23 +138,29 @@ if __name__ == "__main__":
             arrow = cv2.resize(arrow_base, dim_arrow)
 
         # start script
-        conn1, conn2 = Pipe(False)
-        event_stop = Event()
-        p_grab = Process(target=grab, args=(conn2, monitor_bb, event_stop))
-        p_match = Process(target=match, args=(conn1, start, action, arrow))
-        p_grab.start()
-        p_match.start()
-        print("Now running...")
-        while True:
-            event_key = events.get()
-            if (event_key.key == Key.esc or
-                    event_key.key == Key.enter or
-                    str(event_key.key) == "'q'"):
-                break
+        def getMemSizeAndScale():
+            x = numpy.array(mss.mss().grab(monitor_bb))
+            return (x.size * x.itemsize, x.shape, x.dtype)
+
+        with SharedMemoryManager() as smm:
+            mem_size, shape, dtype = getMemSizeAndScale()
+            scr_arr = smm.SharedMemory(mem_size)
+            lock = Lock()
+            event_stop = Event()
+            p_grab = Process(target=grab, args=((scr_arr, shape, dtype), lock, event_stop, monitor_bb))
+            p_match = Process(target=match, args=((scr_arr, shape, dtype), lock, event_stop, start, action, arrow))
+            p_grab.start()
+            p_match.start()
+            print("Now running...")
+            while True:
+                event_key = events.get()
+                if (event_key.key == Key.esc or
+                        event_key.key == Key.enter or
+                        str(event_key.key) == "'q'"):
+                    break
+            
+            event_stop.set()
         print("Stopping Workers...")
-        event_stop.set()
-        conn2.close()
-        conn1.close()
         p_grab.join()
         p_match.join()
     print("Done")
