8201921d977f66bb3e046576a4863b4da56fda56
Klajan <48261494+Klajan@users.noreply.github.com>
2021-08-26 18:14:16 +0200

    Gram smaller area after finding target field

--- AutoScrap.py
+++ AutoScrap.py
@@ -12,6 +12,58 @@ import time
 monitor_number = 1
 # config ends here
 
+def run(monitor_bb, start, action, arrow, event_stop):
+    is_started = False
+    is_found = False
+    monitor = monitor_bb
+    match_width = action.shape[1]
+    arrow_grey = arrow[:,:,2:3]
+    active_keyboard = Controller()
+    with mss.mss() as sct:
+        while not event_stop.is_set():
+            if is_started == True and is_found == True:
+                #scr = numpy.array(sct.grab(monitor))[0:match_y, match_x:match_x+match_width, 2:3]
+                scr = numpy.array(sct.grab(monitor))[:,:,2:3]
+            else:
+                scr = numpy.array(sct.grab(monitor))[:,:,:3]
+            if is_started == False:
+                result = cv2.matchTemplate(scr, start, cv2.TM_CCOEFF_NORMED)
+                _, max_val, _, max_loc = cv2.minMaxLoc(result)
+                if max_val > 0.75:
+                    active_keyboard.press(Key.space)
+                    active_keyboard.release(Key.space)
+                    is_started = True
+            else:
+                if is_found == False:
+                    # find all matches for pattern and select leftmost
+                    result = cv2.matchTemplate(scr, action, cv2.TM_CCOEFF_NORMED)
+                    (y, x) = numpy.where(result > 0.85)
+                    if y.size > 0:
+                        is_found = True
+                        match_x = min(x)
+                        match_y = min(y)
+                        monitor = {
+                            "left": int(monitor_bb["left"] + match_x),
+                            "top": monitor_bb["top"],
+                            "width": int(match_width),
+                            "height": int(match_y),
+                            "mon": monitor_bb["mon"]
+                            }
+                else:
+                    cv2.imwrite("test.png", scr)
+                    # limit search to directly above matched area in red channel and find the arrow
+                    # scr_crop = scr_base[0:match_y, match_x:match_x+match_width, 2:3]
+                    result = cv2.matchTemplate(scr, arrow_grey, cv2.TM_CCOEFF_NORMED)
+                    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+                    if max_val > 0.9:
+                        active_keyboard.press(Key.space)
+                        active_keyboard.release(Key.space)
+                        is_started = False
+                        is_found = False
+                        monitor = monitor_bb
+
+
+
 def grab(shared_mem: tuple, lock, event_stop, monitor):
     with mss.mss() as sct:
         buf = numpy.ndarray(shared_mem[1], shared_mem[2] ,shared_mem[0].buf)
@@ -147,10 +199,12 @@ if __name__ == "__main__":
             scr_arr = smm.SharedMemory(mem_size)
             lock = Lock()
             event_stop = Event()
-            p_grab = Process(target=grab, args=((scr_arr, shape, dtype), lock, event_stop, monitor_bb))
-            p_match = Process(target=match, args=((scr_arr, shape, dtype), lock, event_stop, start, action, arrow))
-            p_grab.start()
-            p_match.start()
+            #p_grab = Process(target=grab, args=((scr_arr, shape, dtype), lock, event_stop, monitor_bb))
+            #p_match = Process(target=match, args=((scr_arr, shape, dtype), lock, event_stop, start, action, arrow))
+            #p_grab.start()
+            #p_match.start()
+            p_run = Process(target=run, args=(monitor_bb , start, action, arrow, event_stop))
+            p_run.start()
             print("Now running...")
             while True:
                 event_key = events.get()
@@ -161,6 +215,7 @@ if __name__ == "__main__":
             
             event_stop.set()
         print("Stopping Workers...")
-        p_grab.join()
-        p_match.join()
+        p_run.join()
+        #p_grab.join()
+        #p_match.join()
     print("Done")
